1f0cd646ca5a97581f2cee6553fcde5410d2fa18
Abuzar Khan <mrabuzar459@gmail.com>
2023-12-07 17:21:47 +0500

    UnCompleted

---
+++ Js/main.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+  });
+  var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
+window.addEventListener("scroll", function(){
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector("navigation");
+
+menuBtn.addEventListener("click", () => {
+  navigation.classList.remove("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  navigation.classList.remove("active");
+});
