4272362375f7c100e099357993c5899a527dd48e
Abuzar Khan <mrabuzar459@gmail.com>
2024-01-02 16:23:46 +0500

    Login SignUp

---
+++ Js/LogSig.js
@@ -0,0 +1,11 @@
+let login = document.querySelector('.login');
+let create = document.querySelector('.create') ;
+let container = document.querySelector('.container');
+
+login.onclick = function() {
+    container.classList.add('signinForm');
+};
+
+create.onclick = function() {
+    container.classList.remove('signinForm');
+};
