91b8b3fce84bdcb130f6277f55f196348c3b3dfe
Abuzar Khan <149166915+Abuzar-Khan2023@users.noreply.github.com>
2024-08-09 20:32:56 +0000

    Navbar adding to ev pg & hambugure

--- Js/Blog.js
+++ Js/Blog.js
@@ -2,6 +2,7 @@
 const blogData = [
 
     {
+      "bannerSrc": "/Images/blog-1.png",
       "topic": "Database",
       "title": "Building microservices with Dropwizard, MongoDB & Docker",
       "description": "This NoSQL database oriented to documents (by documents like JSON) combines some of the features from relational databases, easy to use and the multi-platform is the best option for scale up and have fault tolerance, load balancing, map reduce, etc.",
@@ -10,6 +11,7 @@ const blogData = [
       "duration": "3 min"
     },
     {
+      "bannerSrc": "/Images/blog-2.png",
       "topic": "Web Performance",
       "title": "Fast web page loading on a $20 feature phone",
       "description": "Feature phones are affordable (under $20-25), low-end devices enabling 100s of millions of users in developing countries to leverage the web. Think of them as a light version of a smart phone.",
@@ -18,6 +20,7 @@ const blogData = [
       "duration": "2 min"
     },
     {
+      "bannerSrc": "/Images/blog-3.png",
       "topic": "Accessibility",
       "title": "Accessibility Tips for Web Developers",
       "description": "It's awesome to build sites that are inclusive and accessible to everyone. There are at least six key areas of disability we can optimize for: visual, hearing, mobility, cognition, speech and neural. Many tools and resources can help here, even if you're totally new to web accessibility.",
--- Js/Humburgur.js
+++ Js/Humburgur.js
@@ -1,8 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggle = document.getElementById('toggle');
-  const navMenu = document.getElementById('show-menu');
+document.addEventListener("DOMContentLoaded", function() {
+  const inputBurger = document.getElementById("toggle");
+  const navigation = document.getElementById("show-menu");
+  const menuBtn = document.querySelector(".menuBtn");
+  const closeBtn = document.querySelector(".closeBtn");
 
-  toggle.addEventListener('change', function () {
-      navMenu.classList.toggle('active');
+  inputBurger.addEventListener("change", function() {
+      if (inputBurger.checked) {
+          navigation.style.display = "flex";
+          menuBtn.style.display = "none";
+          closeBtn.style.display = "inline-block";
+      } else {
+          navigation.style.display = "none";
+          closeBtn.style.display = "none";
+          menuBtn.style.display = "inline-block";
+      }
   });
 });
